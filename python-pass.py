e394393d8e692d4a386422a198a1f7d1e8b15377
Aland <32212842+alandio@users.noreply.github.com>
2021-09-28 21:01:25 +0000

    Find the longest palindrome in a string

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,16 @@
-
-
 class Solution:
-
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        long_palindrome = ""
+        for i in range(len(s)):
+            for j in range(len(s),i,-1):
+                if s[i:j] == s[i:j][::-1]:
+                    if len(long_palindrome) < len(s[i:j]):
+                        long_palindrome = s[i:j]
+        return long_palindrome
+
+ob = Solution ()
+print(ob.longest_palindromic("babad"))
+print(ob.longest_palindromic("cbbd"))
+print(ob.longest_palindromic("a"))
+print(ob.longest_palindromic("ac"))
